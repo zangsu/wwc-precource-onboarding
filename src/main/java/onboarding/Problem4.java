09e36e861d142cc172249de6603fa1c00565e890
zangsu <zangsu_@naver.com>
2023-07-29 15:48:17 +0900

    feat : solve problem4 using stream

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,34 @@
 package onboarding;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        HashMap<Character, Character> encoding = new HashMap();
+
+        IntStream lowerCase = IntStream.iterate('a', a -> a <= 'z', a -> a+1);
+        lowerCase.forEach(a -> {
+            char goal = (char)(('z'+'a') - a);
+            encoding.put((char)a, goal);
+        });
+        IntStream upperCase = IntStream.iterate('A', a -> a <= 'Z', a -> a+1);
+        upperCase.forEach(a -> {
+            char goal = (char)(('Z'+'A') - a);
+            encoding.put((char)a, goal);
+        });
+        encoding.put(' ', ' ');
+
+        StringBuilder sb = new StringBuilder();
+        Stream<String> tokens = Arrays.stream(word.split(""));
+        tokens.forEach(s -> {
+            char c = s.charAt(0);
+            sb.append(encoding.get(c));
+        });
+
+
+        return sb.toString();
     }
 }
